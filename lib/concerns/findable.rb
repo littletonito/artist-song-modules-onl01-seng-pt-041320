e65449813011c0b4c105f2b6522cf6d1982ccfb6
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-20 23:36:36 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,10 @@
+module Findable
+  module ClassMethod
+    def find_by_name(name)
+      @@artists.detect{|a| a.name == name}
+    end
+    def find_by_name(name)
+      @@songs.detect{|a| a.name == name}
+    end
+  end
+end
