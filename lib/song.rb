0464a6c334f2b5a8628847831eac55739ba86130
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-20 23:59:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,16 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  
   include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   # def self.find_by_name(name)
